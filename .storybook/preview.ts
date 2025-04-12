268dc14b9fb5cf91ddf0d5b95520f58014405fe7
Brett Smith <brettsmith212@gmail.com>
2025-04-11 20:33:39 -0700

    storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,25 @@
+import type { Preview } from "@storybook/react";
+import "../webviews/index.css";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: '#1e1e1e',
+        },
+      ],
+    },
+  },
+};
+
+export default preview;
